fe675efaee3d21f25b079ebfec529a4a5265ec16
Asif-91 <asifchowdhury323@gmail.com>
2020-10-12 14:21:05 +0600

    project done

--- src/components/VolunteerTasks/VolunteerDetails/VolunteerDetails.js
+++ src/components/VolunteerTasks/VolunteerDetails/VolunteerDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const VolunteerDetails = ({ event, deleteHandler }) => {
 console.log(event.eventId)
@@ -7,15 +7,15 @@ console.log(event.eventId)
     return (
         <div className="col-md-6">
             <div className="shadow p-4 bg-white rounded mb-5 mt-5 card">
-                <div className="row">
+                <div className="row align-items-center">
                     <div className="col-6  ">
-                        <img src={event.pic} className="" alt="" />
+                        <img style={{ height: '180px' }} src={event.eventId.pic} className="" alt="" />
                     </div>
                     <div>
                         <h2>{event.eventId.name} </h2>
                     </div>
                     <br />
-                    <button className="btn btn-primary" onClick={()=>  deleteHandler(event._id) }>Delete</button>
+                    <button className="btn btn-primary mx-auto pt-1" onClick={()=> deleteHandler(event._id)}>Delete</button>
                 </div>
             </div>
         </div>
